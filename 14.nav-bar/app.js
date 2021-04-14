f31ea6b3b31f309aa4d4540180de7ff28aa88c70
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-04-14 14:35:12 +0100

    Added functionality

---
+++ 14.nav-bar/app.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById('nav');
+const toggle = document.getElementById('toggle');
+
+nav.addEventListener('click', function () {
+    nav.classList.toggle('active');
+});
