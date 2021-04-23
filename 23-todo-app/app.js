23e9aaf011b3b01d68ce50748e802cffeee6a54e
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-04-23 23:24:33 +0100

    Added local storage functionality

--- 23-todo-app/app.js
+++ 23-todo-app/app.js
@@ -2,6 +2,12 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUL = document.getElementById('todos');
 
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo));
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -25,15 +31,34 @@ function addTodo(todo) {
 
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed');
+            updateLS();
         });
 
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             todoEl.remove();
+            updateLS();
         })
 
         todosUL.appendChild(todoEl);
 
         input.value = '';
+
+        updateLS();
     }
+}
+
+function updateLS() {
+    todosEl = document.querySelectorAll('li');
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
